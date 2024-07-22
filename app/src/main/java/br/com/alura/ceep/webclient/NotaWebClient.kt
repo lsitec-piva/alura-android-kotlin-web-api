6a5078137744a382e028052b39041d7f1c33a444
lsitec-piva <lucas.piva@lsitec.org.br>
2024-07-22 13:41:30 -0300

    aula 3

---
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -0,0 +1,25 @@
+package br.com.alura.ceep.webclient
+
+import android.util.Log
+import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.services.NotaService
+
+private const val TAG = "NotaWebClient"
+
+class NotaWebClient {
+
+    private val notaService: NotaService = RetrofitInicializador().notaService
+
+    suspend fun buscaTodas(): List<Nota>? {
+        return try {
+            val notasResposta = notaService.buscaTodas()
+            notasResposta.map { notaResposta ->
+                notaResposta.nota
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "buscaTodas: ", e)
+            null
+        }
+    }
+
+}
